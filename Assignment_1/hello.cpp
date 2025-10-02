e61f8413c2fdc9cc66274fd15d250b4cc5ca44fe
Artem <st142163@student.spbu.ru>
2025-10-02 12:47:21 +0300

    Helloworld v2.0 - a program that first says "Hello, world!" and after this reads a string in a cycle and says "Hello, <string>!" while string is not empty.

--- Assignment_1/hello.cpp
+++ Assignment_1/hello.cpp
@@ -1,7 +1,15 @@
 #include "hello.h"
 
 #include <iostream>
+#include <string>
 
-void myproject::sayHello () {
-    std::cout << "Hello, world!" << std::endl;
+void myproject::sayHello() {
+  std::string word = "world";
+  while (true) {
+    std::cout << "Hello, " << word << "!" << std::endl;
+    std::getline(std::cin, word);
+    if (word.empty()) {
+      break;
+    }
+  }
 }
